489404eb7b9322774d3cac84c39cef85903e5133
Tanaky <charp.antoine@gmail.com>
2016-12-08 19:29:20 +0100

    Revert "Corrections TypeCheck.checkType"
    This reverts commit 1b7fb30ce736250d00a2340a722dc3834105785c.

--- src/TypeCheck.java
+++ src/TypeCheck.java
@@ -30,7 +30,7 @@ public class TypeCheck {
 	*/
 	public static boolean checkType(Type expected, Type type) {
 		if (expected == Type.INT)
-				return type == Type.INT || type == Type.POINTER;
+				return type == Type.INT;
 
  		return type == Type.POINTER || type instanceof ArrayType;
 	}
